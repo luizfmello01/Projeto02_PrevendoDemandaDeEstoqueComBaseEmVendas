6b3147bb4211e41b2494871e8f4bf1d7a9fd2346
Luiz Fernando Mello <luizf.mello@hotmail.com>
2021-06-28 17:40:54 -0300

    Create 02_ManipulacaoDosDados.R
    Manipulacao dos dados

---
+++ 02_ManipulacaoDosDados.R
@@ -0,0 +1,59 @@
+#### Parte 2: Manipulação dos dados ####
+
+
+# Novos nomes para as variáveis
+novos_nomes <- c("Semana", "Agencia_ID", "Canal_ID", "Rota_ID", "Cliente_ID",
+                 "Produto_ID", "Quantidade_Vendida_Esta_Semana", "Peso_Vendido_Esta_Semana",
+                 "Quantidade_Reservada_Proxima_Semana", "Peso_Reservado_Proxima_Semana",
+                 "Demanda_Estoque", "ID")
+colnames(vendas) <- novos_nomes
+
+
+# Converter variáveis categóricas para fator
+to.factor <- function(dataframe, colunas) {
+  for( i in colunas  ) {
+    dataframe[[i]] <- as.factor(dataframe[[i]])
+  }
+  return(dataframe)
+}
+
+variaveis_categoricas <- c("Semana", "Agencia_ID", "Canal_ID", "Rota_ID",
+                           "Cliente_ID", "Produto_ID")
+vendas <- to.factor(vendas, variaveis_categoricas)
+
+
+# Visualizar tipo das variaveis do dataframe
+str(vendas)
+
+
+# Remover valores outliers
+vendas_alt <- vendas[!(vendas$ID %in% datasets_outliers$id), -"ID"]
+
+
+# Verificar sumário da variável target
+summary(vendas_alt$Demanda_Estoque)
+# A média da variável target, ficou mais próxima da mediana, já foram removidas os outliers
+
+reserva <- vendas_alt
+
+# Realizar normalização nas variáveis numéricas
+normalizar <- function(x) {
+  return( (x - min(x)) / (max(x) - min(x)) )
+}
+
+variaveis_numericas <- c("Quantidade_Vendida_Esta_Semana", "Peso_Vendido_Esta_Semana",
+                         "Quantidade_Reservada_Proxima_Semana", "Peso_Reservado_Proxima_Semana",
+                         "Demanda_Estoque")
+
+for(i in variaveis_numericas) {
+  vendas_alt[[i]] <- normalizar(vendas_alt[[i]])
+}
+# Resultado: Variáveis númericas foram normalizadas.
+
+
+# Verificar se existe algum valor NA no dataframe
+any(is.na(vendas_alt))
+# Resultado: Não contem nenhum valor NA no dataframe.
+
+
+# Finalizado manipulação dos dados
