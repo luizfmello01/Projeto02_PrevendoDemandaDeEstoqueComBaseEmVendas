40b6cd3ae9228bc026b36594f582c76574aec256
Luiz Fernando Mello <luizf.mello@hotmail.com>
2021-07-01 17:53:55 -0300

    Create 02_ManipulacaoDados.R
    Inicio da manipulação de dados

---
+++ Script_v2/02_ManipulacaoDados.R
@@ -0,0 +1,7 @@
+### Parte 2: Manipulação de dados ###
+# O objetivo dessa parte do script é transformar o dataset
+
+# Padronizar as variáveis numéricas do dataset
+for(i in var.numericas) {
+  # vendas[[i]] <- scale(vendas[[i]], center = 0)
+}
