7263d9671a514aa978e12d6968e2685fcf6adb3c
Luiz Fernando Mello <luizf.mello@hotmail.com>
2021-07-02 23:01:49 -0300

    Update 04_AvaliarModelo.R

--- Script_v2/04_AvaliarModelo.R
+++ Script_v2/04_AvaliarModelo.R
@@ -8,5 +8,5 @@ summary(modelo_v1)
 # Resíduos do modelo preditivo
 residuos <- residuals(modelo_v1)
 summary(residuos)
-# Conclusão: Mediana do residuo, muito próximo de zero, isso é muito bom
-
+# Conclusão: Mediana do residuo, muito próximo de zero, ou seja o modelo
+# aprendeu uma boa relação com os dados
