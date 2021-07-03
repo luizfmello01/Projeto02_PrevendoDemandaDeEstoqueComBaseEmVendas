90372b4d985c40be79aa3b424ddb32098d7a8691
Luiz Fernando Mello <luizf.mello@hotmail.com>
2021-07-02 22:01:04 -0300

    Update 02_ManipulacaoDados.R
    Finalizado manipulação dos dados

--- Script_v2/02_ManipulacaoDados.R
+++ Script_v2/02_ManipulacaoDados.R
@@ -1,7 +1,22 @@
 ### Parte 2: Manipulação de dados ###
 # O objetivo dessa parte do script é transformar o dataset
 
-# Padronizar as variáveis numéricas do dataset
+# Remover valores outliers das variáveis numéricas
 for(i in var.numericas) {
-  # vendas[[i]] <- scale(vendas[[i]], center = 0)
+  qnt <- quantile(vendas[[i]], probs = c(.25, .75), na.rm = TRUE)
+  H <- 1.5 * IQR(vendas[[i]], na.rm = TRUE)
+  vendas[vendas[[i]] < (qnt[1] - H)] <- NA
+  vendas[vendas[[i]] > (qnt[2] + H)] <- NA
 }
+
+vendas <- na.omit(vendas)
+
+
+# Dividir os dados de treino e de teste
+indexTest <- sample.int(nrow(vendas), 100000)
+dados.test <- vendas[indexTest,]
+dados.train <- vendas[-indexTest,]
+rm(vendas)
+
+
+# Conclusão: Fim da manipulação dos dados
